13daa1788f1b96471624074e33654f2b230a8f28
ATS88GR <turok.artyom@gmail.com>
2023-11-15 06:10:17 +0300

    add m4, t2

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,6 +2,8 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
+        String result = (from1UpTo1000 >= 500) ? "Tail" : "Eagle";
+        System.out.println(result);
 
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        int greatest = Math.max(first, second);
+        System.out.println(greatest);
     }
 }
